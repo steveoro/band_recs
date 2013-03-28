6b9f60d02e4033badbf74171ab41b08d900c73cd
Stefano Alloro <steve.alloro@gmail.com>
2013-03-28 10:27:13 +0100

    added yaml require in boot

--- config/boot.rb
+++ config/boot.rb
@@ -1,4 +1,5 @@
 require 'rubygems'
+require 'yaml'
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
@@ -6,4 +7,4 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 require 'bundler/setup' if File.exists?(ENV['BUNDLE_GEMFILE'])
 
 # [Steve, 20130313] Change yamler engine, since Psych (default) can't handle array of symbols:
-YAML::ENGINE.yamler = 'syck'
+YAML::ENGINE.yamler = 'syck' if defined?(YAML::ENGINE)
